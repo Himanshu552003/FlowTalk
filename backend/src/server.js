da5633e2e727d980bdc977da1fe592f99b9cb649
Himanshu552003 <yadavhimanshu552003@gmail.com>
2025-09-03 01:29:24 +0530

    db connected & env.js created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,26 @@
 import express from "express"
 import {ENV} from "./config/env.js"
+import connectDB from "./config/db.js"
 
 import cors from "cors"
 
 
 const app =express()
 
+
+
+connectDB()
+  .then(()=>{
+    app.listen(ENV.PORT,()=>{
+      console.log(`Server is running on port: ${ENV.PORT}`);
+      
+    })
+  })
+  .catch((err)=>{
+    console.error("MongoDb connection error", err);
+    process.exit(1)
+    
+  });
 
 app.get('/',(req, res)=>{
     res.send("Hello ")
