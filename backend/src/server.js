cc7e8b95c799a8705f9a7a59f5541717b2213ee8
Himanshu552003 <yadavhimanshu552003@gmail.com>
2025-09-03 23:48:10 +0530

    feat: integrate Inngest and Clerk for user management
    - Added Inngest as a dependency in the backend.
    - Updated environment configuration to correctly reference STREAM_API_KEY.
    - Enhanced server setup to include Clerk middleware and Inngest event handling.
    - Implemented user synchronization and deletion functions with Inngest.
    - Created a User model for MongoDB to store user data.
    - Integrated Clerk authentication in the frontend with new components.
    - Removed unused CSS files and simplified the App component.
    - Updated package.json files for both frontend and backend to include necessary dependencies.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,15 @@
 import express from "express"
 import {ENV} from "./config/env.js"
 import connectDB from "./config/db.js"
-
-import cors from "cors"
+import { serve } from "inngest/express";
+import {clerkMiddleware} from "@clerk/express"
+import { functions, inngest } from "./config/inngest.js"
 
 
 const app =express()
+app.use(clerkMiddleware())  // req.auth will be available in the req obj
 
-
+app.use(express.json());
 
 connectDB()
   .then(()=>{
@@ -22,10 +24,32 @@ connectDB()
     
   });
 
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.get('/',(req, res)=>{
     res.send("Hello ")
 })
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
-});
+const startServer= async () => {
+  try {
+    await connectDB();
+    if(ENV.NODE_ENV!=="production"){
+      app.listen(ENV.PORT, ()=>{
+        console.log("Server started on port: ", ENV.PORT);
+        
+      });
+    }
+    
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1)
+    
+    
+  }
+  
+};
+
+startServer();
+
+export default app;
+
