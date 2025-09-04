25c99bac4b6c85d3c28853dac8bb004c36ecbc45
Himanshu552003 <yadavhimanshu552003@gmail.com>
2025-09-05 00:05:42 +0530

    feat: integrate Stream Chat and Sentry for enhanced user management and error tracking
    - Added Sentry for error monitoring and tracking with configuration in instrument.mjs.
    - Implemented Stream Chat integration with user upsert and token generation functionalities.
    - Created middleware for route protection and added chat routes for token retrieval.
    - Updated package.json to include necessary dependencies for Sentry and Stream Chat.
    - Refactored Inngest function to upsert Stream user upon user creation.

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,9 @@
+import { json } from "express"
+
+export const protectRoute= (req, res, next)=>{
+    if (!req.auth().isAuthenticated){
+        return res.status(401),json({message: "Unauthorized- you must be logged in "});
+
+    next();
+    }
+}
