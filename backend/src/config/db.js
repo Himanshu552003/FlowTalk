7c3213ba629307fcc90373fb633a488f2242c5d5
Himanshu552003 <yadavhimanshu552003@gmail.com>
2025-09-08 00:21:23 +0530

    feat: integrate Tailwind CSS and Sentry for improved styling and error tracking
    - Added Tailwind CSS and its Vite plugin to the project for enhanced styling capabilities.
    - Refactored App component to utilize Sentry for error tracking and improved routing logic based on user authentication state.
    - Created a new custom hook `useStreamChat` to manage Stream Chat API connections and handle user state.
    - Implemented API call to fetch Stream Chat tokens in a new `api.js` file.
    - Added a new `CallPage` component as a placeholder for future development.
    - Cleaned up global styles by replacing them with Tailwind CSS imports.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose"
 const connectDB= async ()=>{
     try {
        const conn =  await mongoose.connect(ENV.MONGO_URI)
-        // console.log("MongoDB connected successfully:  ", conn.connection.host);
+        console.log("MongoDB connected successfully:  ", conn.connection.host);
         
     } catch (error) {
         console.error("MongoDB connection failed", error);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,36 @@
-import React, { useEffect } from 'react'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
-import {Routes, Route, Navigate} from "react-router"
-import HomePage from './pages/HomePage';
+import * as Sentry from "@sentry/react";
+import { Navigate, Route, Routes } from "react-router";
 import AuthPage from './pages/AuthPage';
-import * as Sentry from "@sentry/react"
+import CallPage from './pages/CallPage';
+import HomePage from './pages/HomePage';
+import { useAuth } from "@clerk/clerk-react";
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
 const App = () => {
 
+  const {isSignedIn, isLoaded}= useAuth()
 
+  if(!isLoaded) return null;
+ 
   return (
+      <SentryRoutes>
+      
+          <Route path='/' element={isSignedIn ? <HomePage/>: <Navigate to ={"/auth"} replace />}/>
+          <Route path='/auth' element={!isSignedIn ? <AuthPage/> : <Navigate to ={"/"} replace />}/>
+      
+          <Route path='/call/:id' element={isSignedIn ? <CallPage/>: <Navigate to ={"/auth"} replace />}/>
 
-    <>
-
-
-      <SignedIn>
-        <SentryRoutes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/auth' element={<Navigate to ={"/"} replace />}/>
+      
+        
+          
+          <Route path='*' element={isSignedIn ? <Navigate to ={"/"} replace /> : <Navigate to ={"/auth"} replace />}/>
         </SentryRoutes>
-      </SignedIn>
-
-      <SignedOut>
-        <SentryRoutes>
-          <Route path='/auth' element={<AuthPage/>}/>
-          <Route path='*' element={<Navigate to ={"/auth"} replace />}/>
-        </SentryRoutes>
-      </SignedOut>
-    </>
+      
+    
   )
 }
 
 export default App
+
+
