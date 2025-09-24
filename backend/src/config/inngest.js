ddb93b29f03a71646cce1892c31122bcb2f0a051
Himanshu552003 <yadavhimanshu552003@gmail.com>
2025-09-24 16:28:02 +0530

    Inngest new app if change

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,49 +1,59 @@
+
 import { Inngest } from "inngest";
-import { connectDB } from "./db.js";
-import { User } from "../models/user.model.js"; // Import the User model
+import {connectDB} from "./db.js";
+import { User } from "../models/user.model.js";
 import { addUserToPublicChannels, deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "slack-clone" });
+export const inngest = new Inngest({ id: "flowtalk" });
+
 
 const syncUser = inngest.createFunction(
   { id: "sync-user" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    await connectDB();
-
-    const { id, email_addresses, first_name, last_name, image_url } = event.data;
-
-    const newUser = {
-      clerkId: id,
-      email: email_addresses[0]?.email_address,
-      name: `${first_name || ""} ${last_name || ""}`,
-      image: image_url,
-    };
-
-    await User.create(newUser);
-
-    await upsertStreamUser({
-      id: newUser.clerkId.toString(),
-      name: newUser.name,
-      image: newUser.image,
-    });
-
-    await addUserToPublicChannels(newUser.clerkId.toString());
+    try {
+      console.log("📝 Heck testing object:");
+      await connectDB();
+      const { id, email_addresses, first_name, last_name, image_url } = event.data;
+      const newUser = {
+        clerkId: id,
+        email: email_addresses?.[0]?.email_address || null,
+        name: `${first_name || ""} ${last_name || ""}`.trim(),
+        image: image_url || null,
+      };
+      console.log("📝 Prepared newUser object:", newUser);
+      const createdUser = await User.create(newUser);
+      console.log("✅ User inserted into Mongo:", createdUser);
+      await upsertStreamUser({
+        id: newUser.clerkId.toString(),
+        name: newUser.name,
+        image: newUser.image,
+      });
+      await addUserToPublicChannels(newUser.clerkId.toString());
+    } catch (err) {
+      console.error("❌ syncUser function error:", err);
+      throw err;
+    }
   }
 );
+
 
 const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
-    await connectDB();
-    const { id } = event.data;
-    await User.deleteOne({ clerkId: id });
-
-    await deleteStreamUser(id.toString());
+    try {
+      await connectDB();
+      const { id } = event.data;
+      await User.deleteOne({ clerkId: id });
+      await deleteStreamUser(id.toString());
+    } catch (err) {
+      console.error("❌ deleteUserFromDB function error:", err);
+      throw err;
+    }
   }
 );
 
-// Create an empty array where we'll export future Inngest functions
+
 export const functions = [syncUser, deleteUserFromDB];
